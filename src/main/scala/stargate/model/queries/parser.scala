a2bc8e6b76ec0015748677ec0a99fca2d3ec9053
Steve Donnelly <steve.donnelly@datastax.com>
2020-05-01 13:22:36 -0400

    renaming validate__ functions to parse__

--- src/main/scala/stargate/model/queries/parser.scala
+++ src/main/scala/stargate/model/queries/parser.scala
@@ -13,26 +13,26 @@ object parser {
   }
 
   // no return, since no transformation is performed
-  def validateGetSelection(entities: Entities, entityName: String, payload: Map[String,Object]): GetSelection = {
+  def parseGetSelection(entities: Entities, entityName: String, payload: Map[String,Object]): GetSelection = {
     val entity = entities(entityName)
     val allowedKeywords = Set(keywords.query.INCLUDE, keywords.pagination.LIMIT, keywords.pagination.CONTINUE, keywords.pagination.TTL)
     val unusedKeys = payload.keySet.diff(entity.relations.keySet ++ allowedKeywords)
     checkUnusedFields(unusedKeys, allowedKeywords, entity.relations.keySet)
     val include = payload.get(keywords.query.INCLUDE).map(_.asInstanceOf[List[String]])
     include.foreach(_.foreach(field => assert(entity.fields.contains(field))))
-    val relations = entity.relations.values.filter(r => payload.contains(r.name)).map(r => (r.name, validateGetSelection(entities, r.targetEntityName, payload(r.name).asInstanceOf[Map[String,Object]])))
+    val relations = entity.relations.values.filter(r => payload.contains(r.name)).map(r => (r.name, parseGetSelection(entities, r.targetEntityName, payload(r.name).asInstanceOf[Map[String,Object]])))
     val limit: Option[Int] = payload.get(keywords.pagination.LIMIT).map(_.asInstanceOf[Integer])
     val continue: java.lang.Boolean = payload.get(keywords.pagination.CONTINUE).map(_.asInstanceOf[java.lang.Boolean]).getOrElse(java.lang.Boolean.FALSE)
     val ttl: Option[Int] = payload.get(keywords.pagination.TTL).map(_.asInstanceOf[Integer])
     GetSelection(relations.toMap, include, limit, continue, ttl)
   }
-  def validateGet(entities: Entities, entityName: String, payload: Map[String,Object]): GetQuery = {
-    val conditions = validateConditions(entities, entityName, payload(keywords.mutation.MATCH))
-    val selection = validateGetSelection(entities, entityName, payload.removed(keywords.mutation.MATCH))
+  def parseGet(entities: Entities, entityName: String, payload: Map[String,Object]): GetQuery = {
+    val conditions = parseConditions(entities, entityName, payload(keywords.mutation.MATCH))
+    val selection = parseGetSelection(entities, entityName, payload.removed(keywords.mutation.MATCH))
     GetQuery(conditions, selection)
   }
 
-  def validateEntity(entities: Entities, entityName: String, payload: Map[String, Object], allowedKeywords: Set[String]): (Map[String,Object], Map[String, RelationMutation]) = {
+  def parseEntity(entities: Entities, entityName: String, payload: Map[String, Object], allowedKeywords: Set[String]): (Map[String,Object], Map[String, RelationMutation]) = {
     assert(!payload.contains(ENTITY_ID_COLUMN_NAME), s"mutation may not specify field: ${ENTITY_ID_COLUMN_NAME}")
     val entity = entities(entityName)
     val allowedFields = entity.fields.keySet ++ entity.relations.keySet
@@ -42,33 +42,33 @@ object parser {
       (field.name, field.scalarType.convert(payload(field.name)))
     })
     val updatedRelations = entity.relations.values.filter(r => payload.contains(r.name)).map(relation => {
-      (relation.name, validateLinkMutation(entities, relation.targetEntityName, payload(relation.name).asInstanceOf[Map[String,Object]]))
+      (relation.name, parseLinkMutation(entities, relation.targetEntityName, payload(relation.name).asInstanceOf[Map[String,Object]]))
     })
     (updatedScalars.toMap, updatedRelations.toMap)
   }
 
-  def validateCreateOne(entities: Entities, entityName: String, payload: Map[String, Object]): CreateOneMutation = {
+  def parseCreateOne(entities: Entities, entityName: String, payload: Map[String, Object]): CreateOneMutation = {
     val entity = entities(entityName)
     val wrappedRelationsPayload: Map[String,Object] = payload.map(kv => {
       val wrapped = if(entity.relations.contains(kv._1)) { Map((keywords.relation.LINK, kv._2)) } else { kv._2 }
       (kv._1, wrapped)
     })
-    val (fields, relations) = validateEntity(entities, entityName, wrappedRelationsPayload, Set.empty)
+    val (fields, relations) = parseEntity(entities, entityName, wrappedRelationsPayload, Set.empty)
     CreateOneMutation(fields, relations.view.mapValues(v => v.asInstanceOf[LinkMutation].mutation).toMap)
   }
 
-  def validateCreate(entities: Entities, entityName: String, payload: Object): CreateMutation = {
+  def parseCreate(entities: Entities, entityName: String, payload: Object): CreateMutation = {
     val createOnes = if (payload.isInstanceOf[List[Map[String, Object]]]) {
-      payload.asInstanceOf[List[Map[String, Object]]].map(validateCreateOne(entities, entityName, _))
+      payload.asInstanceOf[List[Map[String, Object]]].map(parseCreateOne(entities, entityName, _))
     } else if (payload.isInstanceOf[Map[String, Object]]) {
-      List(validateCreateOne(entities, entityName, payload.asInstanceOf[Map[String, Object]]))
+      List(parseCreateOne(entities, entityName, payload.asInstanceOf[Map[String, Object]]))
     } else {
       throw new RuntimeException(s"create request must be either a Map or List[Map], instead got ${payload.getClass}")
     }
     CreateMutation(createOnes)
   }
 
-  def validateConditions[T](validateArgument: (ScalarField, Object) => T, entities: Entities, entityName: String, payload: Object): GroupedConditions[T] = {
+  def parseConditions[T](validateArgument: (ScalarField, Object) => T, entities: Entities, entityName: String, payload: Object): GroupedConditions[T] = {
     def parseCondition(field_op_val: List[Object]): ScalarCondition[Object] = {
       val field :: comparison :: value :: _ = field_op_val
       ScalarCondition(field.asInstanceOf[String], ScalarComparison.fromString(comparison.toString), value)
@@ -93,34 +93,34 @@ object parser {
         | or the string "${keywords.query.MATCH_ALL}", instead got ${payload.getClass}""".stripMargin)
     }
   }
-  def validateConditions(entities: Entities, entityName: String, payload: Object): GroupedConditions[Object] = {
-    validateConditions((field, arg) => field.scalarType.convert(arg), entities, entityName, payload)
+  def parseConditions(entities: Entities, entityName: String, payload: Object): GroupedConditions[Object] = {
+    parseConditions((field, arg) => field.scalarType.convert(arg), entities, entityName, payload)
   }
-  def validateNamedConditions(entities: Entities, entityName: String, payload: Object): GroupedConditions[(ScalarType.Value, String)] = {
-    validateConditions((field, arg) => (field.scalarType, arg.asInstanceOf[String]), entities, entityName, payload)
+  def parseNamedConditions(entities: Entities, entityName: String, payload: Object): GroupedConditions[(ScalarType.Value, String)] = {
+    parseConditions((field, arg) => (field.scalarType, arg.asInstanceOf[String]), entities, entityName, payload)
   }
 
-  def validateUpdate(entities: Entities, entityName: String, payload: Map[String, Object]): UpdateMutation = {
-    val conditions = validateConditions(entities, entityName, payload(keywords.mutation.MATCH))
-    val (fields, relations) = validateEntity(entities, entityName, payload.removed(keywords.mutation.MATCH), Set.empty)
+  def parseUpdate(entities: Entities, entityName: String, payload: Map[String, Object]): UpdateMutation = {
+    val conditions = parseConditions(entities, entityName, payload(keywords.mutation.MATCH))
+    val (fields, relations) = parseEntity(entities, entityName, payload.removed(keywords.mutation.MATCH), Set.empty)
     UpdateMutation(conditions, fields, relations)
   }
 
-  def validateMutation(entities: Entities, entityName: String, payload: Object): Mutation = {
+  def parseMutation(entities: Entities, entityName: String, payload: Object): Mutation = {
     if (payload.isInstanceOf[List[Object]]) {
       // assume create if payload is list
-      validateCreate(entities, entityName, payload)
+      parseCreate(entities, entityName, payload)
     } else if (payload.isInstanceOf[Map[String, Object]]) {
       val payloadMap = payload.asInstanceOf[Map[String, Object]]
       if (payloadMap.keySet == Set(keywords.mutation.CREATE)) {
-        validateCreate(entities, entityName, payloadMap(keywords.mutation.CREATE))
+        parseCreate(entities, entityName, payloadMap(keywords.mutation.CREATE))
       } else if (payloadMap.keySet == Set(keywords.mutation.MATCH)) {
-        MatchMutation(validateConditions(entities, entityName, payloadMap(keywords.mutation.MATCH)))
+        MatchMutation(parseConditions(entities, entityName, payloadMap(keywords.mutation.MATCH)))
       } else if (payloadMap.keySet == Set(keywords.mutation.UPDATE)) {
-        validateUpdate(entities, entityName, payloadMap(keywords.mutation.UPDATE).asInstanceOf[Map[String,Object]])
+        parseUpdate(entities, entityName, payloadMap(keywords.mutation.UPDATE).asInstanceOf[Map[String,Object]])
       } else {
         // default to create
-        validateCreate(entities, entityName, payload)
+        parseCreate(entities, entityName, payload)
       }
     } else {
       throw new RuntimeException(
@@ -128,21 +128,21 @@ object parser {
     }
   }
 
-  def validateLinkMutation(entities: Entities, entityName: String, payload: Map[String,Object]): RelationMutation = {
+  def parseLinkMutation(entities: Entities, entityName: String, payload: Map[String,Object]): RelationMutation = {
     if (payload.isInstanceOf[List[Object]]) {
       // if list, assume replace-create
-      ReplaceMutation(validateMutation(entities, entityName, payload))
+      ReplaceMutation(parseMutation(entities, entityName, payload))
     } else if (payload.isInstanceOf[Map[String, Object]]) {
       val payloadMap = payload.asInstanceOf[Map[String, Object]]
       if(payload.keySet == Set(keywords.relation.LINK)) {
-        LinkMutation(validateMutation(entities, entityName, payloadMap(keywords.relation.LINK)))
+        LinkMutation(parseMutation(entities, entityName, payloadMap(keywords.relation.LINK)))
       } else if(payload.keySet == Set(keywords.relation.UNLINK)) {
-        UnlinkMutation(validateConditions(entities, entityName, payloadMap(keywords.relation.UNLINK)))
+        UnlinkMutation(parseConditions(entities, entityName, payloadMap(keywords.relation.UNLINK)))
       } else if(payload.keySet == Set(keywords.relation.REPLACE)) {
-        ReplaceMutation(validateMutation(entities, entityName, payloadMap(keywords.relation.REPLACE)))
+        ReplaceMutation(parseMutation(entities, entityName, payloadMap(keywords.relation.REPLACE)))
       } else {
         // assume replace if not specified
-        ReplaceMutation(validateMutation(entities, entityName, payloadMap))
+        ReplaceMutation(parseMutation(entities, entityName, payloadMap))
       }
     } else {
       throw new RuntimeException(
@@ -150,18 +150,18 @@ object parser {
     }
   }
 
-  def validateDeleteSelection(entities: Entities, entityName: String, payload: Map[String,Object]): DeleteSelection = {
+  def parseDeleteSelection(entities: Entities, entityName: String, payload: Map[String,Object]): DeleteSelection = {
     val entity = entities(entityName)
     val unusedKeys = payload.keySet.diff(entity.relations.keySet)
     checkUnusedFields(unusedKeys, Set.empty, entity.relations.keySet)
     val relations = entity.relations.values.filter(r => payload.contains(r.name)).map(r => {
-      (r.name, validateDeleteSelection(entities, r.targetEntityName, payload(r.name).asInstanceOf[Map[String,Object]]))
+      (r.name, parseDeleteSelection(entities, r.targetEntityName, payload(r.name).asInstanceOf[Map[String,Object]]))
     })
     DeleteSelection(relations.toMap)
   }
-  def validateDelete(entities: Entities, entityName: String, payload: Map[String,Object]): DeleteQuery = {
-    val conditions = validateConditions(entities, entityName, payload(keywords.mutation.MATCH))
-    val selection = validateDeleteSelection(entities, entityName, payload.removed(keywords.mutation.MATCH))
+  def parseDelete(entities: Entities, entityName: String, payload: Map[String,Object]): DeleteQuery = {
+    val conditions = parseConditions(entities, entityName, payload(keywords.mutation.MATCH))
+    val selection = parseDeleteSelection(entities, entityName, payload.removed(keywords.mutation.MATCH))
     DeleteQuery(conditions, selection)
   }
 
--- src/main/scala/stargate/query/untyped.scala
+++ src/main/scala/stargate/query/untyped.scala
@@ -16,21 +16,21 @@ object untyped {
     }
   }
 
-  val get: (OutputModel, String, Map[String, Object], CqlSession, ExecutionContext) => AsyncList[Map[String, Object]] = validateThenQuery(parser.validateGet, query.get)
+  val get: (OutputModel, String, Map[String, Object], CqlSession, ExecutionContext) => AsyncList[Map[String, Object]] = validateThenQuery(parser.parseGet, query.get)
   def getAndTruncate(model: OutputModel, entityName: String, payload: Map[String,Object], defaultLimit: Int, defaultTTL: Int, session: CqlSession, executor: ExecutionContext): Future[(List[Map[String,Object]], pagination.Streams)] = {
-    query.getAndTruncate(model, entityName, parser.validateGet(model.input.entities, entityName, payload), defaultLimit, defaultTTL, session, executor)
+    query.getAndTruncate(model, entityName, parser.parseGet(model.input.entities, entityName, payload), defaultLimit, defaultTTL, session, executor)
   }
   def getAndTruncate(model: OutputModel, entityName: String, payload: Map[String,Object], defaultLimit: Int, session: CqlSession, executor: ExecutionContext): Future[List[Map[String,Object]]] = {
     getAndTruncate(model, entityName, payload, defaultLimit, 0, session, executor).map(_._1)(executor)
   }
 
-  val createUnbatched: (OutputModel, String, Object, CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.validateCreate, query.createUnbatched)
-  val createBatched: (OutputModel, String, Object, CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.validateCreate, query.createBatched)
+  val createUnbatched: (OutputModel, String, Object, CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.parseCreate, query.createUnbatched)
+  val createBatched: (OutputModel, String, Object, CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.parseCreate, query.createBatched)
 
-  val updateUnbatched: (OutputModel, String, Map[String, Object], CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.validateUpdate, query.updateUnbatched)
-  val updateBatched: (OutputModel, String, Map[String, Object], CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.validateUpdate, query.updateBatched)
+  val updateUnbatched: (OutputModel, String, Map[String, Object], CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.parseUpdate, query.updateUnbatched)
+  val updateBatched: (OutputModel, String, Map[String, Object], CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.parseUpdate, query.updateBatched)
 
-  val deleteUnbatched: (OutputModel, String, Map[String, Object], CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.validateDelete, query.deleteBatched)
-  val deleteBatched: (OutputModel, String, Map[String, Object], CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.validateDelete, query.deleteUnbatched)
+  val deleteUnbatched: (OutputModel, String, Map[String, Object], CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.parseDelete, query.deleteBatched)
+  val deleteBatched: (OutputModel, String, Map[String, Object], CqlSession, ExecutionContext) => Future[List[Map[String, Object]]] = validateThenQuery(parser.parseDelete, query.deleteUnbatched)
 
 }
