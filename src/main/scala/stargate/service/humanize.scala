c76b73f926e31739861a24746c2a6fc93b629dfb
Ryan Svihla <rs@foundev.pro>
2020-05-04 18:01:00 +0200

    fixes to startup (#46)
    * command line interface with documentation with -h flag
    * using maven shade to concat all existing application.conf and
      refrence.conf to prevent collisions
    * default configuration file names changed to avoid conflicts
    * can now accept environment variables for configuration
    * formatting for various files
    * contact points accept env variable string

--- src/main/scala/stargate/service/humanize.scala
+++ src/main/scala/stargate/service/humanize.scala
@@ -8,9 +8,9 @@ object humanize {
   def bytes(bytes: Long): String = {
     if (bytes < 1024) {
       s"$bytes b"
-    } else if (bytes < MB){
+    } else if (bytes < MB) {
       f"${bytes.toFloat / KB.toFloat}%.2f kb"
-    } else if (bytes < GB){
+    } else if (bytes < GB) {
       f"${bytes.toFloat / MB.toFloat}%.2f mb"
     } else {
       f"${bytes.toFloat / GB.toFloat}%.2f gb"
