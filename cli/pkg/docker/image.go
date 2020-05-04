9fcec8826be05178b75b979ec5eb4b32f80063e4
Matt Scandalis <scandalis.matthew@gmail.com>
2020-05-04 11:26:11 -0700

    more testing cleaner image code

--- cli/pkg/docker/image.go
+++ cli/pkg/docker/image.go
@@ -1,10 +1,33 @@
 package docker
 
 import (
+	"errors"
 	"strings"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 )
+
+// GetNameWithVersion finds a full versioned name for an image
+func (client *Client) GetNameWithVersion(image string) (string, error) {
+	ctx := client.ctx
+	cli := client.cli
+
+	args := filters.NewArgs()
+	args.Add("reference", image)
+	summary, err := cli.ImageList(ctx, types.ImageListOptions{
+		Filters: args,
+	})
+	if err != nil {
+		return "", err
+	}
+	for _, r := range summary {
+		if len(r.RepoTags) > 0 && strings.Index(r.RepoTags[0], image+":") == 0 {
+			return r.RepoTags[0], nil
+		}
+	}
+	return "", errors.New("Could not find a matching image")
+}
 
 // EnsureImage makes sure that the image we need is present and returns the correct name
 func (client *Client) EnsureImage(dockerHost, image string) (string, error) {
@@ -16,16 +39,16 @@ func (client *Client) EnsureImage(dockerHost, image string) (string, error) {
 		if image != "service" {
 			return "", err
 		}
-		summary, err2 := cli.ImageList(ctx, types.ImageListOptions{})
+		name, err2 := client.GetNameWithVersion("stargate")
 		if err2 != nil {
 			return "", err
 		}
-		for _, r := range summary {
-			if len(r.RepoTags) > 0 && strings.Index(r.RepoTags[0], "stargate:") == 0 {
-				return r.RepoTags[0], nil
-			}
-		}
+		return name, nil
 	}
 	defer reader.Close()
-	return image, nil
+	name, err := client.GetNameWithVersion(image)
+	if err != nil {
+		return "", err
+	}
+	return name, nil
 }
