934e6ec0f5f70941463b61253cb19283a1c1e456
Ryan Svihla <rs@foundev.pro>
2020-05-05 20:32:00 +0200

    Trace logs (#62)
    * dockerfile improvements
    * info in prod, trace in dev
    * actively kill docker container when tests are done

--- kube/build-docker.sh
+++ kube/build-docker.sh
@@ -10,18 +10,16 @@ fi
 date_suffix=$(date "+%y%m%d-%H%M%S")
 clone_path="./stargate-clone-${date_suffix}"
 
-git clone "." "$clone_path"
+git clone --depth 1 file:///`pwd` "$clone_path"
+ln -vs $clone_path stargate-clone
 pushd "$clone_path"
 mvn package
 if [ $? -ne 0 ]; then
     exit 1
 fi
 popd
-ln -s "$clone_path" "./stargate-clone"
 
 docker build "." -f "./kube/Dockerfile" -t "stargate:1.0"
 
 rm -f "./stargate-clone"
-
-
-
+rm -fr $clone_path
