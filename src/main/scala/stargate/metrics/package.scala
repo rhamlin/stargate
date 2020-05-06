4d36dff1c14a6f7ae74b420ba0a2dadbfc3eec40
Ryan Svihla <rs@foundev.pro>
2020-05-06 12:40:31 +0200

    apache license and file fixes (#65)
    * applied apache header to all but .json files with do not support
      annotation
    * fixed several typos
    * removed unused imports

--- src/main/scala/stargate/metrics/package.scala
+++ src/main/scala/stargate/metrics/package.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package stargate
 
 import org.eclipse.jetty.server.handler.StatisticsHandler
@@ -6,6 +22,7 @@ import io.prometheus.client.hotspot.DefaultExports
 import org.eclipse.jetty.server.Server
 
 package object metrics {
+
   /**
   * sets default Hotspot metrics for the JVM
   */
@@ -18,11 +35,11 @@ package object metrics {
     * it gets the previous handler and wraps it
     * @param server Jetty Server
     */
-  def registerServletHandlerStastics(server: Server): Unit = {
+  def registerServletHandlerStatistics(server: Server): Unit = {
     // Configure StatisticsHandler.
     val stats = new StatisticsHandler()
     //get current server handler and set it inside StatisticsHandler
-    stats.setHandler(server.getHandler())
+    stats.setHandler(server.getHandler)
     //set StatisticsHandler as the handler for servlet
     server.setHandler(stats)
     // Register collector.
--- src/test/scala/stargate/query/ReadWriteTestTrait.scala
+++ src/test/scala/stargate/query/ReadWriteTestTrait.scala
@@ -1,13 +1,29 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package stargate.query
 
-import stargate.{CassandraTest, CassandraTestSession, cassandra, model}
 import com.typesafe.config.{Config, ConfigFactory}
-import org.junit.{AfterClass, BeforeClass, Test}
+import org.junit.Test
+import stargate.schema.ENTITY_ID_COLUMN_NAME
+import stargate.{CassandraTestSession, cassandra, model}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.jdk.CollectionConverters._
-import stargate.schema.ENTITY_ID_COLUMN_NAME
 
 trait ReadWriteTestTrait extends CassandraTestSession {
 
