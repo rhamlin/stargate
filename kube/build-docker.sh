fc56ad1e35a54ddca661b153c8620d5951c40287
Ryan Svihla <rs@foundev.pro>
2020-05-06 17:52:18 +0200

    publishing docker images (#64)
    * simplified build
    * publishing to github packages due to permissions
    * downgraded cassandra java driver to 4.3.0
    * tag image with git reference on push

--- kube/build-docker.sh
+++ kube/build-docker.sh
@@ -11,26 +11,5 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-script_path="./kube/build-docker.sh" 
-if [ ! "$BASH_SOURCE" == "$script_path" ]; then
-    echo "script must be ran as: $script_path"
-    exit 1 
-fi
-
-
-date_suffix=$(date "+%y%m%d-%H%M%S")
-clone_path="./stargate-clone-${date_suffix}"
-
-git clone --depth 1 file:///`pwd` "$clone_path"
-ln -vs $clone_path stargate-clone
-pushd "$clone_path"
-mvn package
-if [ $? -ne 0 ]; then
-    exit 1
-fi
-popd
-
-docker build "." -f "./kube/Dockerfile" -t "stargate:1.0"
-
-rm -f "./stargate-clone"
-rm -fr $clone_path
+mvn clean package -DskipTests=true
+docker build "." -f "./kube/Dockerfile" -t "stargate:latest"
