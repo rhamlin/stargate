350abc740f56bc8b72d7fb612ba8cb77642247e8
Steve Donnelly <steve.donnelly@datastax.com>
2020-05-14 19:03:10 -0400

    adding start of generating python demo

--- src/main/scala/stargate/notebook/package.scala
+++ src/main/scala/stargate/notebook/package.scala
@@ -3,8 +3,27 @@ package stargate
 import com.typesafe.config.ConfigFactory
 import stargate.model.{Entities, InputModel, generator}
 
+import scala.util.Try
+
 package object notebook {
 
+  case class DemoData(entityA: String, as: List[Map[String,Object]], relationName: String, entityB: String, bs: List[Map[String,Object]])
+
+  def generateDemo(model: Entities, count: Int): Try[DemoData] = {
+    Try({
+      val possibleRelations = model.values.toList.map(e => {
+        val hasScalars = e.fields.size > 1
+        val relationsWithScalars = e.relations.values.filter(r => model(r.targetEntityName).fields.size > 1)
+        (e.name, Option.when(hasScalars)(()).flatMap(_ => relationsWithScalars.headOption))
+      })
+      val chosenRelation = possibleRelations.filter(_._2.isDefined).head
+      val a = chosenRelation._1
+      val relation = chosenRelation._2.get.name
+      val b = chosenRelation._2.get.targetEntityName
+      def generate(e: String): List[Map[String, Object]] = List.range(0, count).map(_ => generator.entityFields(model(e).fields.values.toList))
+      DemoData(a, generate(a), relation, b, generate(b))
+    })
+  }
 
   def pythonCrudFuncs(stargateHost: String, appName: String, entityName: String, op: String): String = {
     val method = op match {
@@ -27,6 +46,20 @@ package object notebook {
        |
        |""".stripMargin
   }
+
+  def pythonDemo(stargateHost: String, appName: String, demo: DemoData): String = {
+    val a_ids = s"${demo.entityA}_ids"
+    val b_ids = s"${demo.entityB}_ids"
+    s"""
+       |${a_ids} = parseResponse(requests.post("http://${stargateHost}/${appName}/${demo.entityA}", json=${util.toPrettyJson(demo.as)}))
+       |print(${a_ids})
+       |
+       |${b_ids} = parseResponse(requests.post("http://${stargateHost}/${appName}/${demo.entityB}", json=${util.toPrettyJson(demo.bs)}))
+       |print(${b_ids})
+       |""".stripMargin
+  }
+
+
 
   def markdownCell(text: String): Map[String,Object] = {
     Map[String,Object](
@@ -57,7 +90,9 @@ package object notebook {
         pythonCrudFuncs(stargateHost, appName, entity.name, op)
       })
     }).mkString("\n"))
-    util.scalaToJava(notebook.updated("cells", initCells ++ List(libraryCell, headerCell)))
+    val demoCells = generateDemo(model, 3).map(d => List(codeCell(pythonDemo(stargateHost, appName, d)))).getOrElse(List.empty)
+    val cells = initCells ++ List(libraryCell, headerCell) ++ demoCells
+    util.scalaToJava(notebook.updated("cells", cells))
   }
 
 
