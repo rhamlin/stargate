d577c9c0c40eca85e8f2f7e01b64622a6ecd312a
Steve Donnelly <steve.donnelly@datastax.com>
2020-05-14 17:04:48 -0400

    first draft of python lib rewrite

--- src/main/scala/stargate/notebook/package.scala
+++ src/main/scala/stargate/notebook/package.scala
@@ -6,23 +6,27 @@ import stargate.model.{Entities, InputModel, generator}
 package object notebook {
 
 
-  def pythonCrudReq(stargateHost: String, appName: String, entityName: String, op: String): String = {
-    s"""request = requests.get("http://${stargateHost}/${appName}/generator/${entityName}/${op}").json()
-       |print("example ${op} request for entity ${entityName}:\\n" + json.dumps(request,indent=4))
-       |""".stripMargin
-  }
-  def pythonCrudResp(stargateHost: String, appName: String, entityName: String, op: String): String = {
+  def pythonCrudFuncs(stargateHost: String, appName: String, entityName: String, op: String): String = {
     val method = op match {
       case "create" => "post"
       case "get" => "get"
       case "update" => "put"
       case "delete" => "delete"
     }
-    s"""response = requests.${method}("http://${stargateHost}/${appName}/${entityName}", json=request).json()
-       |print("response for ${op} ${entityName}:\\n" + json.dumps(response,indent=4))
+    val parseFunc = if(op == keywords.config.query.op.GET) "" else "Mutation"
+    val requestFuncName = s"${op}Random${entityName}Request"
+    val opFuncName = s" ${op}${entityName}"
+    s"""def ${requestFuncName}():
+       |    parseResponse(requests.get("http://${stargateHost}/${appName}/generator/${entityName}/${op}"))
+       |
+       |def ${opFuncName}(request):
+       |    parse${parseFunc}Response(requests.${method}("http://${stargateHost}/${appName}/${entityName}", json=request))
+       |
+       |def ${op}Random${entityName}():
+       |    ${opFuncName}(${requestFuncName}())
+       |
        |""".stripMargin
   }
-
 
   def markdownCell(text: String): Map[String,Object] = {
     Map[String,Object](
@@ -37,18 +41,16 @@ package object notebook {
     cell.updated("source", List(code))
   }
 
+
   def pythonNotebook(stargateHost: String, appName: String, model: Entities): Object = {
     val notebook = util.javaToScala(ConfigFactory.parseResources("jupyter/python/empty-notebook.conf").resolve.root.unwrapped).asInstanceOf[Map[String,Object]]
     val initCells = util.javaToScala(ConfigFactory.parseResources("jupyter/python/init-cells.conf").resolve.getValue("init_cells").unwrapped).asInstanceOf[List[Object]]
-    val cells = model.values.flatMap(entity => {
-      keywords.config.query.crudOps.toList.flatMap(op => {
-        val md = markdownCell(s"""# ${op} ${entity.name}""")
-        val req = codeCell(pythonCrudReq(stargateHost, appName, entity.name, op))
-        val resp = codeCell(pythonCrudResp(stargateHost, appName, entity.name, op))
-        List(md, req, resp)
+    val library = model.values.flatMap(entity => {
+      keywords.config.query.crudOps.toList.map(op => {
+        pythonCrudFuncs(stargateHost, appName, entity.name, op)
       })
-    })
-    util.scalaToJava(notebook.updated("cells", initCells ++ cells))
+    }).mkString("\n")
+    util.scalaToJava(notebook.updated("cells", initCells ++ codeCell(library)))
   }
 
 
