217d776190520f165de755dc768b3dfacf285919
Steve Donnelly <steve.donnelly@datastax.com>
2020-05-18 00:38:23 -0400

    moving python-specific notebook generation to separate module

--- src/main/scala/stargate/notebook/package.scala
+++ src/main/scala/stargate/notebook/package.scala
@@ -3,10 +3,9 @@ package stargate
 import java.util.stream.Collectors
 
 import com.typesafe.config.ConfigFactory
+import stargate.model.{Entities, ScalarComparison, generator}
 
 import scala.jdk.CollectionConverters._
-import stargate.model.{Entities, InputModel, ScalarComparison, generator}
-
 import scala.util.{Random, Try}
 
 package object notebook {
@@ -53,81 +52,6 @@ package object notebook {
   def getById(id: String, relations: List[String], limit: Option[Int], continue: Boolean): String = get(idEqCondition(id), relations, limit, continue)
 
 
-  def pythonCrudFuncs(stargateHost: String, appName: String, entityName: String, op: String): String = {
-    val method = op match {
-      case "create" => "post"
-      case "get" => "get"
-      case "update" => "put"
-      case "delete" => "delete"
-    }
-    val parseFunc = if(op == keywords.config.query.op.GET) "" else "Mutation"
-    val requestFuncName = s"${op}Random${entityName}Request"
-    val opFuncName = s"${op}${entityName}"
-    s"""def ${requestFuncName}():
-       |    return parseResponse(requests.get("http://${stargateHost}/${appName}/generator/${entityName}/${op}"))
-       |
-       |def ${opFuncName}(request):
-       |    return parse${parseFunc}Response(requests.${method}("http://${stargateHost}/${appName}/${entityName}", json=request))
-       |
-       |def ${op}Random${entityName}():
-       |    return ${opFuncName}(${requestFuncName}())
-       |
-       |""".stripMargin
-  }
-
-  def pythonDemoCode(stargateHost: String, appName: String, demo: DemoData): DemoCode = {
-    val a_resp = s"${demo.entityA}_response"
-    val b_resp = s"${demo.entityB}_response"
-    val a_ids = s"${demo.entityA}_ids"
-    val b_ids = s"${demo.entityB}_ids"
-    val create1 = s"""${a_resp} = requests.post("http://${stargateHost}/${appName}/${demo.entityA}", json=${util.toPrettyJson(demo.as)})
-       |${a_ids} = parseMutationResponse(${a_resp})
-       |print("${demo.entityA} ids:" + str(${a_ids}))
-       |
-       |${b_resp} = requests.post("http://${stargateHost}/${appName}/${demo.entityB}", json=${util.toPrettyJson(demo.bs)})
-       |${b_ids} = parseMutationResponse(${b_resp})
-       |print("${demo.entityB} ids:" + str(${b_ids}))
-       |""".stripMargin
-    val linkRequest1: String = indent(linkByEntityIdRequest(s"${a_ids}[i]", demo.relationName, s"${b_ids}[i]")).strip
-    val link1 =
-      s"""for i in range(${demo.as.length}):
-         |  requests.put("http://${stargateHost}/${appName}/${demo.entityA}", json=${linkRequest1})
-         |""".stripMargin
-    val getRequest1: String = get(idInCondition(s"${a_ids}"), List(demo.relationName), Some(demo.as.length), false)
-    val get1 =
-      s"""response = requests.get("http://${stargateHost}/${appName}/${demo.entityA}", json=${getRequest1})
-         |print(json.dumps(parseResponse(response), indent=2))
-         |""".stripMargin
-    val duplicateValue = util.toJson(demo.bs(0)(demo.duplicateField))
-    val link2Condition = conditionString(demo.duplicateField, ScalarComparison.EQ, duplicateValue)
-    val linkRequest2 =
-      s"""{
-         |  "${keywords.mutation.MATCH}": ${idEqCondition(s"${a_ids}[0]")},
-         |  "${demo.relationName}": { "${keywords.relation.REPLACE}": { "${keywords.mutation.MATCH}": ${link2Condition} } }
-         |}""".stripMargin
-    val link2 =
-      s"""response = requests.put("http://${stargateHost}/${appName}/${demo.entityA}", json=${linkRequest2})
-         |print(json.dumps(parseResponse(response), indent=2))
-         |""".stripMargin
-    val getRequest2: String = get(idInCondition(s"${a_ids}"), List(demo.relationName), Some(1), true)
-    val get2 =
-      s"""page=0
-         |response = parseResponse(requests.get("http://${stargateHost}/${appName}/${demo.entityA}", json=${getRequest2}))
-         |print("Page: " + str(page))
-         |print(json.dumps(response, indent=2))
-         |while len(response) == 2:
-         |  page = page + 1
-         |  response = parseResponse(requests.get("http://${stargateHost}/${appName}/continue/" + response[1]["${keywords.pagination.CONTINUE}"]))
-         |  print("Page: " + str(page))
-         |  print(json.dumps(response, indent=2))
-         |""".stripMargin
-    val cleanup =
-      s"""requests.delete("http://${stargateHost}/${appName}/${demo.entityA}", json={ "${keywords.mutation.MATCH}": ${idInCondition(a_ids)} })
-         |requests.delete("http://${stargateHost}/${appName}/${demo.entityB}", json={ "${keywords.mutation.MATCH}": ${idInCondition(b_ids)} })
-         |""".stripMargin
-    DemoCode(create1, link1, get1, link2, get2, cleanup)
-  }
-
   def demoCells(demo: DemoData, demoCode: DemoCode): List[Map[String,Object]] = {
     val createMarkdown = markdownCell(s"""### Create some instances of ${demo.entityA} and ${demo.entityB}""")
     val createCode = codeCell(demoCode.create1)
@@ -147,6 +71,9 @@ package object notebook {
   }
 
 
+  def headerCell: Map[String,Object] = {
+    util.javaToScala(ConfigFactory.parseResources("jupyter/header-cell.conf").resolve.root.unwrapped).asInstanceOf[Map[String,Object]]
+  }
   def markdownCell(text: String): Map[String,Object] = {
     Map[String,Object](
       ("cell_type", "markdown"),
@@ -155,9 +82,6 @@ package object notebook {
     )
   }
 
-  def headerCell: Map[String,Object] = {
-    util.javaToScala(ConfigFactory.parseResources("jupyter/header-cell.conf").resolve.root.unwrapped).asInstanceOf[Map[String,Object]]
-  }
   def codeCell(code: String): Map[String,Object] = {
     val cell = util.javaToScala(ConfigFactory.parseResources("jupyter/empty-code-cell.conf").resolve.root.unwrapped).asInstanceOf[Map[String,Object]]
     cell.updated("source", List(code))
@@ -166,20 +90,4 @@ package object notebook {
     val cell = util.javaToScala(ConfigFactory.parseResources("jupyter/empty-init-cell.conf").resolve.root.unwrapped).asInstanceOf[Map[String,Object]]
     cell.updated("source", List(code))
   }
-
-
-  def pythonNotebook(stargateHost: String, appName: String, model: Entities): Object = {
-    val notebook = util.javaToScala(ConfigFactory.parseResources("jupyter/python/empty-notebook.conf").resolve.root.unwrapped).asInstanceOf[Map[String,Object]]
-    val initCells = util.javaToScala(ConfigFactory.parseResources("jupyter/python/init-cells.conf").resolve.getValue("init_cells").unwrapped).asInstanceOf[List[Object]]
-    val libraryCell = initCell(model.values.flatMap(entity => {
-      keywords.config.query.crudOps.toList.map(op => {
-        pythonCrudFuncs(stargateHost, appName, entity.name, op)
-      })
-    }).mkString("\n"))
-    val _demoCells = generateDemo(model, 3).map(d => demoCells(d, pythonDemoCode(stargateHost, appName, d))).getOrElse(List.empty)
-    val cells = initCells ++ List(libraryCell, headerCell) ++ _demoCells
-    util.scalaToJava(notebook.updated("cells", cells))
-  }
-
-
 }
---
+++ src/main/scala/stargate/notebook/python.scala
@@ -0,0 +1,98 @@
+package stargate.notebook
+
+import com.typesafe.config.ConfigFactory
+import stargate.model.{Entities, ScalarComparison}
+import stargate.{keywords, util}
+
+object python {
+
+  def pythonCrudFuncs(stargateHost: String, appName: String, entityName: String, op: String): String = {
+    val method = op match {
+      case "create" => "post"
+      case "get" => "get"
+      case "update" => "put"
+      case "delete" => "delete"
+    }
+    val parseFunc = if(op == keywords.config.query.op.GET) "" else "Mutation"
+    val requestFuncName = s"${op}Random${entityName}Request"
+    val opFuncName = s"${op}${entityName}"
+    s"""def ${requestFuncName}():
+       |    return parseResponse(requests.get("http://${stargateHost}/${appName}/generator/${entityName}/${op}"))
+       |
+       |def ${opFuncName}(request):
+       |    return parse${parseFunc}Response(requests.${method}("http://${stargateHost}/${appName}/${entityName}", json=request))
+       |
+       |def ${op}Random${entityName}():
+       |    return ${opFuncName}(${requestFuncName}())
+       |
+       |""".stripMargin
+  }
+
+
+  def pythonDemoCode(stargateHost: String, appName: String, demo: DemoData): DemoCode = {
+    val a_resp = s"${demo.entityA}_response"
+    val b_resp = s"${demo.entityB}_response"
+    val a_ids = s"${demo.entityA}_ids"
+    val b_ids = s"${demo.entityB}_ids"
+    val create1 = s"""${a_resp} = requests.post("http://${stargateHost}/${appName}/${demo.entityA}", json=${util.toPrettyJson(demo.as)})
+                     |${a_ids} = parseMutationResponse(${a_resp})
+                     |print("${demo.entityA} ids:" + str(${a_ids}))
+                     |
+                     |${b_resp} = requests.post("http://${stargateHost}/${appName}/${demo.entityB}", json=${util.toPrettyJson(demo.bs)})
+                     |${b_ids} = parseMutationResponse(${b_resp})
+                     |print("${demo.entityB} ids:" + str(${b_ids}))
+                     |""".stripMargin
+    val linkRequest1: String = indent(linkByEntityIdRequest(s"${a_ids}[i]", demo.relationName, s"${b_ids}[i]")).strip
+    val link1 =
+      s"""for i in range(${demo.as.length}):
+         |  requests.put("http://${stargateHost}/${appName}/${demo.entityA}", json=${linkRequest1})
+         |""".stripMargin
+    val getRequest1: String = get(idInCondition(s"${a_ids}"), List(demo.relationName), Some(demo.as.length), false)
+    val get1 =
+      s"""response = requests.get("http://${stargateHost}/${appName}/${demo.entityA}", json=${getRequest1})
+         |print(json.dumps(parseResponse(response), indent=2))
+         |""".stripMargin
+    val duplicateValue = util.toJson(demo.bs(0)(demo.duplicateField))
+    val link2Condition = conditionString(demo.duplicateField, ScalarComparison.EQ, duplicateValue)
+    val linkRequest2 =
+      s"""{
+         |  "${keywords.mutation.MATCH}": ${idEqCondition(s"${a_ids}[0]")},
+         |  "${demo.relationName}": { "${keywords.relation.REPLACE}": { "${keywords.mutation.MATCH}": ${link2Condition} } }
+         |}""".stripMargin
+    val link2 =
+      s"""response = requests.put("http://${stargateHost}/${appName}/${demo.entityA}", json=${linkRequest2})
+         |print(json.dumps(parseResponse(response), indent=2))
+         |""".stripMargin
+    val getRequest2: String = get(idInCondition(s"${a_ids}"), List(demo.relationName), Some(1), true)
+    val get2 =
+      s"""page=0
+         |response = parseResponse(requests.get("http://${stargateHost}/${appName}/${demo.entityA}", json=${getRequest2}))
+         |print("Page: " + str(page))
+         |print(json.dumps(response, indent=2))
+         |while len(response) == 2:
+         |  page = page + 1
+         |  response = parseResponse(requests.get("http://${stargateHost}/${appName}/continue/" + response[1]["${keywords.pagination.CONTINUE}"]))
+         |  print("Page: " + str(page))
+         |  print(json.dumps(response, indent=2))
+         |""".stripMargin
+    val cleanup =
+      s"""requests.delete("http://${stargateHost}/${appName}/${demo.entityA}", json={ "${keywords.mutation.MATCH}": ${idInCondition(a_ids)} })
+         |requests.delete("http://${stargateHost}/${appName}/${demo.entityB}", json={ "${keywords.mutation.MATCH}": ${idInCondition(b_ids)} })
+         |""".stripMargin
+    DemoCode(create1, link1, get1, link2, get2, cleanup)
+  }
+
+
+  def pythonNotebook(stargateHost: String, appName: String, model: Entities): Object = {
+    val notebook = util.javaToScala(ConfigFactory.parseResources("jupyter/python/empty-notebook.conf").resolve.root.unwrapped).asInstanceOf[Map[String,Object]]
+    val initCells = util.javaToScala(ConfigFactory.parseResources("jupyter/python/init-cells.conf").resolve.getValue("init_cells").unwrapped).asInstanceOf[List[Object]]
+    val libraryCell = initCell(model.values.flatMap(entity => {
+      keywords.config.query.crudOps.toList.map(op => {
+        pythonCrudFuncs(stargateHost, appName, entity.name, op)
+      })
+    }).mkString("\n"))
+    val _demoCells = generateDemo(model, 3).map(d => demoCells(d, pythonDemoCode(stargateHost, appName, d))).getOrElse(List.empty)
+    val cells = initCells ++ List(libraryCell, headerCell) ++ _demoCells
+    util.scalaToJava(notebook.updated("cells", cells))
+  }
+}
